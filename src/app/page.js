88fbfd6e7e0bd19427a7b5945e60e4ac5688b13f
irfaan008 <irfan@geeksinvention.com>
2025-06-26 16:53:16 +0530

    image updaetd

--- src/app/page.js
+++ src/app/page.js
@@ -11,7 +11,7 @@ const activities = [
     dates: "13ᵗʰ Jan 2024 - 26ᵗʰ Jan 2024",
     time: "10:00 a.m. - 7:00 p.m.",
     location: "Hong Kong, Kwun Tong",
-    image: "/kayak.jpg",
+    image: "https://images.unsplash.com/photo-1440993443326-9e9f5aea703a?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8a2F5YWt8ZW58MHx8MHx8fDI%3D",
   },
   {
     activity: "YOGA",
@@ -20,7 +20,7 @@ const activities = [
     dates: "13ᵗʰ Jan 2024 - 26ᵗʰ Jan 2024",
     time: "10:00 a.m. - 7:00 p.m.",
     location: "Hong Kong, Kwun Tong",
-    image: "/yoga.jpg",
+    image: "https://images.unsplash.com/photo-1510894347713-fc3ed6fdf539?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTN8fHlvZ2F8ZW58MHx8MHx8fDI%3D",
   },
   {
     activity: "FITNESS",
@@ -29,7 +29,7 @@ const activities = [
     dates: "13ᵗʰ Jan 2024 - 26ᵗʰ Jan 2024",
     time: "10:00 a.m. - 7:00 p.m.",
     location: "Hong Kong, Kwun Tong",
-    image: "/fitness.jpg",
+    image: "https://images.unsplash.com/photo-1483721310020-03333e577078?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8RklUTkVTU3xlbnwwfHwwfHx8Mg%3D%3D",
   },
   {
     activity: "RUNNING",
@@ -38,7 +38,7 @@ const activities = [
     dates: "13ᵗʰ Jan 2024 - 26ᵗʰ Jan 2024",
     time: "10:00 a.m. - 7:00 p.m.",
     location: "Hong Kong, Kwun Tong",
-    image: "/running.jpg",
+    image: "https://images.unsplash.com/photo-1590333748338-d629e4564ad9?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OXx8cnVubmluZ3xlbnwwfHwwfHx8Mg%3D%3D",
   },
   {
     activity: "CYCLING",
@@ -47,7 +47,7 @@ const activities = [
     dates: "13ᵗʰ Jan 2024 - 26ᵗʰ Jan 2024",
     time: "10:00 a.m. - 7:00 p.m.",
     location: "Hong Kong, Kwun Tong",
-    image: "/cycling.jpg",
+    image: "https://images.unsplash.com/photo-1534787238916-9ba6764efd4f?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8Y3ljbGluZ3xlbnwwfHwwfHx8Mg%3D%3D",
   },
   {
     activity: "HIKING",
@@ -56,7 +56,7 @@ const activities = [
     dates: "13ᵗʰ Jan 2024 - 26ᵗʰ Jan 2024",
     time: "10:00 a.m. - 7:00 p.m.",
     location: "Hong Kong, Kwun Tong",
-    image: "/hiking.jpg",
+    image: "https://images.unsplash.com/photo-1465311440653-ba9b1d9b0f5b?w=900&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fGhpa2luZ3xlbnwwfHwwfHx8Mg%3D%3D",
   },
 ];
 
