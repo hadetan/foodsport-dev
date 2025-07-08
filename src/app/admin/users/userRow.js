52de1af2cb397cb71ade77c00896a2ad49b4f260
Madni-Coder <madnicoder@gmail.com>
2025-07-08 18:26:52 +0530

    removed svg from user table (#26)

--- src/app/admin/users/userRow.js
+++ src/app/admin/users/userRow.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Status from "@/app/admin/users/status";
 import Avatar from "@/app/admin/users/avatar";
+import {Pencil,Eye} from 'lucide-react'
 
 export const UserRow = ({ user }) => {
     return (
@@ -54,45 +55,13 @@ export const UserRow = ({ user }) => {
                                 router.push(`/admin/users/${user.id}`)
                             }
                         >
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                className="h-4 w-4"
-                                fill="none"
-                                viewBox="0 0 24 24"
-                                stroke="currentColor"
-                            >
-                                <path
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    strokeWidth="2"
-                                    d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
-                                />
-                                <path
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    strokeWidth="2"
-                                    d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z"
-                                />
-                            </svg>
+                            <Pencil/>
                         </button>
                         <button
                             className="btn btn-sm btn-ghost tooltip"
                             data-tip="Edit User"
                         >
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                className="h-4 w-4"
-                                fill="none"
-                                viewBox="0 0 24 24"
-                                stroke="currentColor"
-                            >
-                                <path
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    strokeWidth="2"
-                                    d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z"
-                                />
-                            </svg>
+                            <Eye/>
                         </button>
                     </div>
                 </td>
