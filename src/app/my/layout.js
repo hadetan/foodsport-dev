e09848851ab88662426fd46b7d3bcd9bc0664694
hadetan <aquibsyed83@gmail.com>
2025-07-28 16:37:10 +0530

    Remove: activities provider from my layout

--- src/app/my/layout.js
+++ src/app/my/layout.js
@@ -1,6 +1,5 @@
 import Header from '@/app/shared/components/Header';
 import Footer from '@/app/shared/components/footer';
-import { ActivitiesProvider } from '@/app/shared/contexts/ActivitiesContext';
 import { UserProvider } from '@/app/shared/contexts/userContext';
 
 export default async function MyLayout({ children }) {
@@ -8,9 +7,7 @@ export default async function MyLayout({ children }) {
 		<>
 			<Header />
 			<UserProvider>
-				<ActivitiesProvider>
 					<main className='flex-grow'>{children}</main>
-				</ActivitiesProvider>
 			</UserProvider>
 			<Footer />
 		</>
