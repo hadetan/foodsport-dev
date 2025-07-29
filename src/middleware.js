50c5021b6062c6953cfd514df7b8df77c127ebbb
hadetan <aquibsyed83@gmail.com>
2025-07-29 18:32:58 +0530

    Resolved issue of loggedin and non-loggedin users clicking opening url that are opposite of their states

--- src/middleware.js
+++ src/middleware.js
@@ -4,14 +4,26 @@ export function middleware(request) {
   const token = request.cookies.get('auth_token');
   const url = request.nextUrl;
 
+  // If not logged in and visiting /my/activities/:id, redirect to /activities/:id
+  if (!token && url.pathname.startsWith('/my/activities/')) {
+    const newPath = url.pathname.replace(/^\/my/, '');
+    return NextResponse.redirect(new URL(newPath + url.search, request.url));
+  }
+
+  // If logged in and visiting /activities/:id, redirect to /my/activities/:id
+  if (token && url.pathname.startsWith('/activities/')) {
+    const newPath = '/my' + url.pathname;
+    return NextResponse.redirect(new URL(newPath + url.search, request.url));
+  }
+
   if (token) {
     // If logged in and visiting landing page, redirect to /my
     if (url.pathname === '/') {
       return NextResponse.redirect(new URL('/my', request.url));
     }
   } else {
-    // If not logged in and visiting /my, redirect to /login
-    if (url.pathname.startsWith('/my')) {
+    // If not logged in and visiting /my (but not /my/activities), redirect to /auth/login
+    if (url.pathname.startsWith('/my') && !url.pathname.startsWith('/my/activities/')) {
       return NextResponse.redirect(new URL('/auth/login', request.url));
     }
   }
@@ -19,5 +31,5 @@ export function middleware(request) {
 }
 
 export const config = {
-  matcher: ['/', '/my/:path*'],
+  matcher: ['/', '/my/:path*', '/activities/:path*'],
 };
