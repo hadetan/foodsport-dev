96d007077242e102a42b6d81067d8b41bc0dc9d3
madni coder <madnicoder@gmail.com>
2025-08-01 20:05:16 +0530

    Dashboard creation

--- src/app/(landing)/Components/ComingSoon.js
+++ src/app/(landing)/Components/ComingSoon.js
@@ -1,14 +1,9 @@
+import "./comingSoon.css";
+
 export default function ComingSoon() {
     return (
-      <div className="flex flex-col items-center justify-center min-h-screen bg-yellow-100">
-        <img
-          src="/coming.jpg"
-          alt="Coming Soon"
-          width={1784}
-          height={770}
-          className="object-contain"
-        />
-      </div>
+        <div className="coming-soon-bg">
+            {/* You can add overlay text or content here if needed */}
+        </div>
     );
-  }
-  
+}
