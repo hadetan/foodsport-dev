95b0889910eac66cc0d7f342190e3a9236a8442c
madni coder <madnicoder@gmail.com>
2025-08-12 18:01:03 +0530

    Enhance CreateActivityPage with validation and error handling for form fields

--- src/app/admin/(logged_in)/activities/page.js
+++ src/app/admin/(logged_in)/activities/page.js
@@ -49,8 +49,7 @@ function ActivityManagementPageContent() {
         "Location",
         "Capacity",
         "Status",
-        "Actions",
-    ];
+        "Actions",    ];
     const getActivities = async () => {
         try {
             setTableLoading(true);
