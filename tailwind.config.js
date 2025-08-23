6dccf44a524f561587dfa4a30c6e5ba24aea7cd4
madni coder <madnicoder@gmail.com>
2025-08-23 20:35:02 +0530

    Fixed theme on macos

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+// tailwind.config.js
+module.exports = {
+    plugins: [require("daisyui")],
+    daisyui: {
+        themes: ["light"], // Only enable the light theme
+        darkTheme: false, // Explicitly disable dark mode
+    },
+};
