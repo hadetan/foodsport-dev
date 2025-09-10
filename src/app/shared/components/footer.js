26fb19548ee336b36a275f5e37269f3759b15919
Madni-Coder <madnicoder@gmail.com>
2025-09-10 18:28:59 +0530

    View in social media

--- src/app/shared/components/footer.js
+++ src/app/shared/components/footer.js
@@ -105,7 +105,7 @@ export default function Footer() {
                                                 fontWeight: "bold",
                                             }}
                                         >
-                                            View Image
+                                            View In Social Media
                                         </div>
                                     )}
                                 </a>
