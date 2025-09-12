94ec766888e6aab929f5c7170f1c0ca62b36e5a8
hadetan <aquibsyed83@gmail.com>
2025-09-12 15:35:25 +0530

    Added html in admin layout

--- src/app/admin/layout.js
+++ src/app/admin/layout.js
@@ -1,7 +1,14 @@
 'use client';
+import '@/app/globals.css'
 
 export default function AdminLoginLayout({ children }) {
 	//check if user is logged in or not, if not then send them to /admin/login page forcefully.
 
-	return <div className='min-h-screen bg-base-100'>{children}</div>;
+	return (
+		<html>
+			<body>
+				<div className='min-h-screen bg-base-100'>{children}</div>;
+			</body>
+		</html>
+	)
 }
