c04940332a08330919c8bd6f3e07342cb07b8680
madni coder <madnicoder@gmail.com>
2025-09-17 14:21:53 +0530

    added text rewards not image

--- src/app/[locale]/(landing)/redeem/page.js
+++ src/app/[locale]/(landing)/redeem/page.js
@@ -3,6 +3,7 @@
 import { useMemo, useState } from "react";
 import RewardCards from "../Components/rewardCards";
 import Image from "next/image";
+import redeemTitle from "@/app/shared/components/redeemTitle";
 
 const categories = [
     { key: "food", label: "Food & Beverages" },
@@ -83,25 +84,22 @@ export default function RedeemPage() {
         <div className="min-h-screen bg-[#E5E1DB]">
             {/* Rewards banner image */}
             <div className="w-full px-0 ">
-                <Image
-                    src="/rewardsImage.png"
-                    alt="Rewards banner"
-                    width={1242}
-                    height={770}
-                    className="w-full h-auto rounded-2xl object-cover shadow"
-                    priority
-                />
+            {redeemTitle()}
+
+             
+
+
             </div>
 
             {/* Header watermark */}
-            <div className="relative overflow-hidden bg-white py-10">
-                <div className="container mx-auto px-4 flex justify-center">
-                    <h1 className="uppercase font-extrabold tracking-[0.25em] text-[10vw] leading-none text-gray-800 sm:text-7xl lg:text-8xl select-none text-center">
-                        Rewards
+            <div className="relative overflow-hidden">
+                <div className="container mx-auto px-4 pt-10 pb-6 flex justify-center">
+                    <h1 className="uppercase font-extrabold tracking-[0.25em] text-[10vw] leading-none text-white  sm:text-7xl lg:text-8xl select-none text-center">
+                        Redeem Rewards
                     </h1>
                 </div>
+                <div className="h-px bg-[#C9C3BB]/70" />
             </div>
-            <div className="h-px bg-[#C9C3BB]/70" />
 
             {/* Filters row */}
             <div className="container mx-auto px-4">
---
+++ src/app/shared/components/redeemTitle.js
@@ -0,0 +1,15 @@
+import styles from "@/app/shared/css/redeem.module.css";
+
+export default function redeemTitle() {
+    return (
+        <section className={styles.redeemBg}>
+            <div className={styles.overlay}>
+                <div className={styles.centerContent}>
+                    <span className={styles.line}></span>
+                    <h1 className={styles.title}>Rewards</h1>
+                    <span className={styles.line}></span>
+                </div>
+            </div>
+        </section>
+    );
+}
