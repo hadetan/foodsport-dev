05e0ca9cc0629c9fa4be1e38f21b62bab9f5f444
hadetan <aquibsyed83@gmail.com>
2026-01-13 18:13:50 +0530

    fix: update environment variable for server API base URL

--- src/utils/axios/serverApi.js
+++ src/utils/axios/serverApi.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { DEFAULT_TIMEOUT } from './config';
 
 const serverApi = axios.create({
-  baseURL: process.env.NEXT_SERVER_BASEURL,
+  baseURL: process.env.SERVER_BASEURL,
   timeout: DEFAULT_TIMEOUT,
   headers: {
     'Content-Type': 'application/json',
